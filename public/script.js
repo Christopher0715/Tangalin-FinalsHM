9120c372b3fa9835d184c733315f83e5cb809a9a
UBSITTangalinChristopherBoy <97861000+Christopher0715@users.noreply.github.com>
2022-05-16 10:47:07 +0800

    finals

---
+++ public/script.js
@@ -0,0 +1,8 @@
+//this is innHTMl sample
+
+   /* multi-Line comments in JS
+   document.getElementById("person").innerHTML="Lyndsy"; is like this */
+   console.log("Hello, world!")
+   console.log(5+23)
+   // ---------- call an alert from the script instead of the console ----------
+   alert("Hello Welcome to my Site. You can dismiss me by clicking in the OK button.")
